27af2c3f3d3fabc1a04f1aa2cadd5d70d5d117e9
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2026-04-15 23:26:42 +0200

    feat(vizz-map): wire public exports

--- packages/vizz-map/src/index.ts
+++ packages/vizz-map/src/index.ts
@@ -1,1 +1,9 @@
-export {}
+export { LayerManager } from './layer-manager'
+export type { LayerManagerProps, LayerItem } from './layer-manager'
+
+export { LayerManagerItem } from './layer-manager-item'
+export type { LayerManagerItemProps } from './layer-manager-item'
+
+export { useAnchorLayerId } from './use-anchor-layer-id'
+
+export { findAnchorLayerId, VIZZ_ANCHOR_MARKERS } from './utils'
